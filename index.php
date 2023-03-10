580aaa8d786632cc0c3fa44087db54903ab517f3
Daniil <94884910+Filienko@users.noreply.github.com>
2023-03-09 17:38:16 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,189 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Bitnami LAMP</title>
-    <meta name="description" content="Bitnami: Open Source. Simplified.">
-    <meta name="author" content="Bitnami">
-    <link rel="stylesheet" media="screen" href="//unpkg.com/@bitnami/hex/dist/hex.min.css">
-  </head>
-  <body>
-    <main class="margin-t-huge">
-      <section aria-labelledby="installation-title" aria-describedby="installation-desc" class="container container-tiny margin-b-giant">
-        <h1 id="installation-title">Congratulations!</h1>
-        <div aria-hidden="true" style="height: 4px; width: 100%;" class="gradient-135-brand"></div>
-        <p id="installation-desc" class="type-big">You are now running <strong>Bitnami LAMP 8.1.14</strong> in the Cloud.</p>
-      </section>
-      <section aria-labelledby="links-title" aria-describedby="links-desc" class="bg-light padding-v-bigger margin-v-enormous">
-        <div class="container container-tiny">
-          <div class="row row-collapse-b-tablet align-center ">
-            <div class="col-6">
-              <h3 id="links-title" class="margin-t-reset">Useful Links</h3>
-              <p id="links-desc" class="margin-b-reset">The following links will help you to understand better how to get started and configure the application you just launched.</p>
-            </div>
-            <div class="col-6">
-              <ul class="remove-style margin-reset">
-                <li class="padding-v-normal"><a href="https://docs.bitnami.com/google/infrastructure/lamp/get-started/get-started/" target="_blank" style="width: 100%;" class="button text-c button-accent">Get Started</a></li>
-                <li class="padding-v-normal"><a href="https://docs.bitnami.com/google/infrastructure/lamp/get-started/access-phpmyadmin/" target="_blank" style="width: 100%;" class="button text-c button-primary">Connect to phpMyAdmin</a></li>
-                <li class="padding-v-normal"><a href="https://docs.bitnami.com/google/infrastructure/lamp/" target="_blank" style="width: 100%;" class="button text-c button-primary">Documentation</a></li>
-                <li class="padding-v-normal"><a href="https://github.com/bitnami/vms" target="_blank" style="width: 100%;" class="button text-c button-primary">Support</a></li>
-              </ul>
-            </div>
+<html>
+<head>
+<title>W3.CSS Template</title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+.myLink {display: none}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Navigation Bar -->
+<div class="w3-bar w3-white w3-border-bottom w3-xlarge">
+  <a href="#" class="w3-bar-item w3-button w3-text-red w3-hover-red"><b><i href=""></i>ParcelPortal</b></a>
+  <a href="#" class="w3-bar-item w3-button w3-right w3-hover-red w3-text-grbley"><i class="fa fa-search"></i></a>
+</div>
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-hide-small" style="max-width:1500px">
+    <img class="w3-image" src="/BG_w.jpg" alt="Background" width="1500" height="700">
+  <div class="w3-display-middle" style="width:65%">
+    <div class="w3-bar w3-black">
+      <button class="w3-bar-item w3-button tablink" onclick="openLink(event, 'investor');"><i class="fa fa-map-marker w3-margin-right"></i>Invest</button>
+      <button class="w3-bar-item w3-button tablink" onclick="openLink(event, 'owner');"><i class="fa fa-bed w3-margin-right"></i>Evaluate your House</button>
+      <button class="w3-bar-item w3-button tablink" onclick="openLink(event, 'buyer');"><i class="fa fa-car w3-margin-right"></i>Buy your first House</button>
+      <button class="w3-bar-item w3-button tablink" onclick="openLink(event, 'city');"><i class="fa fa-car w3-margin-right"></i>City Staff</button>
+    </div>
+
+    <!-- Tabs -->
+    <div id="investor" class="w3-container w3-white w3-padding-16 myLink">
+      <h3>Invest with us</h3>
+          <label>County (Optional)</label>
+          <input class="w3-input w3-border" type="text" placeholder="King, Pierce, etc">
+        <p><a class="w3-button w3-dark-grey"  href="investor.php">Search investment opportunities</a></p>
+      </div>
+
+
+    <div id="buyer" class="w3-container w3-white w3-padding-16 myLink">
+      <h3>Find the best houses in a city!</h3>
+      <p><button class="w3-button w3-dark-grey">Search Houses</button></p>
+    </div>
+
+    <div id="owner" class="w3-container w3-white w3-padding-16 myLink">
+      <h3>Find information about your house!</h3>
+      <input class="w3-input w3-border" type="text" placeholder="The number of your house (optional)">
+        <p><a class="w3-button w3-dark-grey"  href="investor.php">Proceed to the database</a></p>
+      </div>
+
+     <div id="city" class="w3-container w3-white w3-padding-16 myLink">
+      <h3>Find the information about your city!</h3>
+        <p><a class="w3-button w3-dark-grey"  href="investor.php">Update the information about your city</a></p>
+      </div>
+    </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content" style="max-width:1100px;">
+
+  <!-- Good offers -->
+  <div class="w3-container w3-margin-top">
+    <h3>Good Offers Right Now</h3>
+    <h6><strong>Best</strong> Houses in Washington.</h6>
+  </div>
+  <div class="w3-row-padding w3-text-white w3-large">
+    <div class="w3-half w3-margin-bottom">
+      <div class="w3-display-container">
+        <img src="/k1.jpg" alt="Cinque Terre" style="width:100%">
+        <span class="w3-display-bottomleft w3-padding">King County</span>
+      </div>
+    </div>
+    <div class="w3-half">
+      <div class="w3-row-padding" style="margin:0 -16px">
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+            <img src="/land_p.jpg" alt="New York" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">Pierce County</span>
           </div>
         </div>
-      </section>
-
-    </main>
-    <footer class="text-c">
-      <div class="container container-tiny margin-v-enormous">
-        <hr class="separator separator-small margin-b-bigger">
-        <a class="light" href="https://bitnami.com/" aria-label="Go to the Bitnami site" target="_blank"><svg width="42" height="47" viewBox="0 0 126 141" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>Bitnami logo</title><defs><path d="M7.187 75.559C2.698 72.968.301 70.402.301 65.32V7.476c0-2.643.705-5.022 1.845-7.034l24.345 13.99v72.273L7.187 75.56z" id="ba"/><linearGradient x1="18.01%" y1="80.098%" x2="77.804%" y2="28.779%" id="c"><stop stop-color="#00437C" offset="0%"/><stop stop-color="#093266" offset="100%"/></linearGradient><path d="M8.673 47.68L.45 42.932V12.691l15.23 8.792L51.908.566V.432L76.35 14.478c-1.178 2.14-2.937 3.913-5.25 5.248L22.7 47.671a14.004 14.004 0 0 1-7.02 1.902 14 14 0 0 1-7.006-1.893" id="d"/><linearGradient x1="0%" y1="50%" y2="50%" id="f"><stop stop-color="#00437C" offset="0%"/><stop stop-color="#3984B5" offset="100%"/></linearGradient></defs><g fill="none" fill-rule="evenodd"><path d="M78.432 61.949l-15.206-8.78-15.206 8.78 15.205 8.865z" fill="#00577B"/><path d="M63.226 88.292l15.206-8.782V61.948L63.226 70.73z" fill="#1E384B"/><path d="M63.225 88.292L48.02 79.51V61.948l15.205 8.782z" fill="#15211F"/><path d="M99.455 50.024v41.704l-36.23 20.917-36.233-20.917v-.063l-24.345 13.99c1.276 2.251 3.097 4.043 5.113 5.207 16.067 9.276 32.132 18.553 48.2 27.828 4.715 2.724 9.79 2.737 14.48.03 16.104-9.3 32.212-18.599 48.316-27.897 4.51-2.604 6.889-6.736 6.889-11.99V43.242c0-2.736-.597-5.175-1.748-7.264L99.455 50.024z" fill="#00437C" fill-rule="nonzero"/><g><g transform="matrix(1 0 0 -1 .502 106.097)"><mask id="bb" fill="#fff"><use xlink:href="#ba"/></mask><g mask="url(#bb)" fill="url(#c)" fill-rule="nonzero"><path d="M7.187 75.559C2.698 72.968.301 70.402.301 65.32V7.476c0-2.643.705-5.022 1.845-7.034l24.345 13.99v72.273L7.187 75.56z"/></g></g></g><g><g transform="matrix(1 0 0 -1 47.547 50.457)"><mask id="e" fill="#fff"><use xlink:href="#d"/></mask><g mask="url(#e)" fill="url(#f)" fill-rule="nonzero"><path d="M8.673 47.68L.45 42.932V12.691l15.23 8.792L51.908.566V.432L76.35 14.478c-1.178 2.14-2.937 3.913-5.25 5.248L22.7 47.671a14.004 14.004 0 0 1-7.02 1.902 14 14 0 0 1-7.006-1.893"/></g></g></g></g></svg></a>
-        <p class="type-small margin-reset">Proudly built by Bitnami</p>
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+            <img src="/p2.jpg" alt="San Francisco" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">Pierce County</span>
+          </div>
+        </div>
       </div>
-    </footer>
-  </body>
+      <div class="w3-row-padding" style="margin:0 -16px">
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+              <img src="/land_k.jpg" alt="Pisa" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">King County</span>
+          </div>
+        </div>
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+            <img src="/p3.jpg" alt="Paris" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">Pierce County</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Explore Nature -->
+  <div class="w3-container">
+    <h3>Explore Washington Housing Market </h3>
+    <p> Look at these amazing opportunities.</p>
+  </div>
+  <div class="w3-row-padding">
+    <div class="w3-half w3-margin-bottom">
+      <img src="/se1.jpg" alt="King County House" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Seattle, King </h3>
+        <p class="w3-opacity">Evaluated at 1.2 million dollars</p>
+        <p>Beautiful house outside of Seattle.</p>
+        <button class="w3-button w3-margin-bottom">Get Report</button>
+      </div>
+    </div>
+    <div class="w3-half w3-margin-bottom">
+      <img src="/tacoma_land.jpg" alt="Austria" style="width:100%">
+      <div class="w3-container w3-white">
+        <h3>Tacoma, Pierce</h3>
+        <p class="w3-opacity">Evaluated at 250000 dollars</p>
+        <p>Piece of land perfectly fitted for the first house for a young family in Tacoma metro.</p>
+        <button class="w3-button w3-margin-bottom">Get Report</button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Contact -->
+  <div class="w3-container">
+    <h2>Contact</h2>
+    <p>Let us find your next home!</p>
+    <i class="fa fa-map-marker" style="width:30px"></i> Seattle, WA<br>
+    <i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515<br>
+    <i class="fa fa-envelope" style="width:30px"> </i> Email: mail@mail.com<br>
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name"></p>
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Email" required name="Email"></p>
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message"></p>
+      <p><button class="w3-button w3-black w3-padding-large" type="submit">SEND MESSAGE</button></p>
+    </form>
+  </div>
+  
+<!-- End page content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-center w3-opacity w3-margin-bottom">
+  <h5>Find Us On</h5>
+  <div class="w3-xlarge w3-padding-16">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+</footer>
+
+<script>
+// Tabs
+function openLink(evt, linkName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("myLink");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(linkName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+// Click on the first tablink on load
+document.getElementsByClassName("tablink")[0].click();
+</script>
+
+</body>
 </html>
