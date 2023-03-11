016be9b036da08fdaa60ba86fc235e806092a4b8
Daniil <94884910+Filienko@users.noreply.github.com>
2023-03-10 22:48:37 -0800

    Update city.php

--- city/city.php
+++ city/city.php
@@ -5,104 +5,83 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Investment Opportunities</title>
+        <link rel="icon" type="image/png" href="/logo.jpeg"/>
+        <title>ParcelPortal</title>
         <!-- add a reference to the external stylesheet -->
-        <link rel="stylesheet" href="https://bootswatch.com/4/solar/bootstrap.min.css">
+        <link rel="stylesheet" href="https://bootswatch.com/4/journal/bootstrap.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+
+            body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+            .myLink {display: none}
+        </style>
     </head>
     <body>
         <!-- START -- Add HTML code for the top menu section (navigation bar) -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
-            <a class="navbar-brand" href="#">Navbar</a> 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria- 
-                    controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation"> 
-                <span class="navbar-toggler-icon"></span> 
-            </button> 
+        <div class="w3-bar w3-white w3-border-bottom w3-xlarge">
+            <a href="index.php" class="w3-bar-item w3-button w3-text-blue w3-hover-blue"><b>ParcelPortal</b></a>
+        </div>
 
-            <div class="collapse navbar-collapse" id="navbarColor02"> 
-                <ul class="navbar-nav mr-auto"> 
-                </li> 
-                    <li class="nav-item"> 
-                        <a class="nav-link active" href="employee.php">Invest</a> 
-                    </li> 
-                    <li class="nav-item"> 
-                        <a class="nav-link" href="department.php">Evaluate your House</a> 
-                    </li> 
-                    <li class="nav-item"> 
-                        <a class="nav-link" href="project.php">Buy new House</a> 
-                    </li> 
-                    <li class="nav-item"> 
-                        <a class="nav-link" href="project.php">City Staff</a> 
-                    </li> 
-                   <!-- <li class="nav-item dropdown"> 
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria- 
-                           haspopup="true" aria-expanded="false">Dropdown</a> 
-                        <div class="dropdown-menu"> 
-                            <a class="dropdown-item" href="#">Action</a> 
-                            <a class="dropdown-item" href="#">Another action</a> 
-                            <a class="dropdown-item" href="#">Something else here</a> 
-                            <div class="dropdown-divider"></div> 
-                            <a class="dropdown-item" href="#">Separated link</a> 
-                        </div> 
-                    </li> -->
-                </ul> 
-                <form class="form-inline my-2 my-lg-0"> 
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search"> 
-                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button> 
-                </form> 
-            </div> 
-        </nav>
         <!-- END -- Add HTML code for the top menu section (navigation bar) -->
-        <div class="jumbotron"> 
-            <p class="lead">You can use this website to execute SQL queries on COMPANY Database <p> 
-            <hr class="my-4"> 
-            <form method="GET" action="city.php"> 
-                <select name="emp" onchange='this.form.submit()'> 
-                    <option selected>Select a name</option> 
+        <div class="jumbotron">
+            <p class="lead"> This section is intended for City clerk inquiries, credentials not required.<p>
+            <hr class="my-4">
+            <form method="GET" action="city.php">
+                <select name="city" onchange='this.form.submit()'>
+                    <option selected> Select taxpayer ID </option>
 
-                    <?php 
+                    <?php
                     //Saving the connection through a set of global variables from config file
                     //establishing the credentials and testuser name necessery for connecting to the DB hosted on VM
                     //if mistake is found in connection process, the connection is not continued
-                    $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME); 
-                    if ( mysqli_connect_errno() )  
-                    { 
-                        die( mysqli_connect_error() );   
+                    $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+                    if ( mysqli_connect_errno() )
+                    {
+                        die( mysqli_connect_error() );
                     } 
-                    $sql = "select Lname, Fname, SSN from EMPLOYEE"; 
+                    $sql = "SELECT Taxpayer_ID FROM taxpayer";
                     //Saving the location of the project after the sql statement through storing it in a row variable
                     // where $ is analogous to a var keyword from JS in php-->
 
-                    if ($result = mysqli_query($connection, $sql))  
-                    { 
+                    if ($result = mysqli_query($connection, $sql))
+                    {
                         //Stores query results as part of a row variable
-                        // loop through the data 
-                        while($row = mysqli_fetch_assoc($result)) 
-                        { 
-                            echo '<option value="' . $row['SSN'] . '">'; 
-                            echo $row['Lname']. ', '. $row['Fname'];  
-                            echo "</option>"; 
-                        } 
-                        // release the memory used by the result set 
-                        mysqli_free_result($result);  
-                    }  
+                        // loop through the data
+                        while($row = mysqli_fetch_assoc($result))
+                        {
+                            echo '<option value="' . $row['Taxpayer_ID'] . '">';
+                            echo $row['Taxpayer_ID'];
+                            echo "</option>";
+                        }
+                        // release the memory used by the result set
+                        mysqli_free_result($result);
+                    }
                     ?>  
                 </select> 
+
                 <?php 
                 if ($_SERVER["REQUEST_METHOD"] == "GET")  
                 { 
-                    if (isset($_GET['emp']) )  
+                    if (isset($_GET['city']) )  
                     { 
                 ?> 
                 <p>&nbsp;</p> 
                 <table class="table table-hover"> 
                     <thead> 
                         <tr class="table-success"> 
-                            <th scope="col">Last Name</th> 
-                            <th scope="col">First Name</th> 
-                            <th scope="col">Social Security #</th> 
-                            <th scope="col">Salary</th> 
-                            <th scope="col">Birth Date</th> 
-                            <th scope="col">Department</th> 
+                            <th scope="col">Tax ID </th> 
+                            <th scope="col">Parcel ID </th>
+                            <th scope="col">Property types </th> 
+                            <th scope="col">First </th> 
+                            <th scope="col">Last </th> 
+                            <th scope="col">Street </th> 
+                            <th scope="col">City </th> 
+                            <th scope="col">Zip </th> 
+                            <th scope="col">Land Value </th> 
+                            <th scope="col">% Tax change </th> 
+
                         </tr> 
                     </thead> 
                     <?php            
@@ -110,10 +89,16 @@
                         { 
                             die( mysqli_connect_error() );   
                         } 
-                        $sql = "  SELECT *  
-                            FROM EMPLOYEE, DEPARTMENT  
-                            WHERE SSN = {$_GET['emp']} AND     
-                                  EMPLOYEE.Dno = DEPARTMENT.Dnumber"; 
+
+
+                        $sql =  "select T.Taxpayer_ID TAXID,  I.parcel_ID PID, B.Property_type PTYPE, 
+                                    T.fname FN, T.lname LN, T.street S, T.city TCITY,
+                                    T.zip Z, I.Assessed_land_val LVAL, I.Pct_Tax_Change PCTTAX
+                                FROM taxpayer T
+                                JOIN landparcel L on T.Taxpayer_ID = L.Taxpayer_ID
+                                JOIN taxinfo I on L.Parcel_ID = I.Parcel_ID
+                                JOIN building B on I.Parcel_ID = B.Parcel_ID
+                                WHERE T.Taxpayer_ID = '{$_GET['city']}' ";
 
                         if ($result = mysqli_query($connection, $sql))  
                         { 
@@ -121,15 +106,19 @@
                             { 
                     ?> 
                     <tr> 
-                        <td><?php echo $row['Lname'] ?></td> 
-                        <td><?php echo $row['Fname'] ?></td> 
-                        <td><?php echo $row['Ssn'] ?></td> 
-                        <td><?php echo $row['Salary'] ?></td> 
-                        <td><?php echo $row['Bdate'] ?></td> 
-                        <td><?php echo $row['Dname'] ?></td> 
-                    </tr> 
-                    <?php 
-                            } 
+                        <td><?php echo $row['TAXID'] ?></td> 
+                        <td><?php echo $row['PID'] ?></td>
+                        <td><?php echo $row['PTYPE'] ?></td>
+                        <td><?php echo $row['FN'] ?></td> 
+                        <td><?php echo $row['LN'] ?></td>
+                        <td><?php echo $row['S'] ?></td>
+                        <td><?php echo $row['TCITY'] ?></td> 
+                        <td><?php echo $row['Z'] ?></td>
+                        <td><?php echo $row['LVAL'] ?></td> 
+                        <td><?php echo $row['PCTTAX'] ?></td>
+                    </tr>
+                    <?php
+                            }
                             // release the memory used by the result set 
                             mysqli_free_result($result);  
                         }  
